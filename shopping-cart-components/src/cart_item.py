4b278ab2811c3290bc7cce825b19aec4bae4ba58
uruz <uruzutkuun@gmail.com>
2026-09-16 09:04:33 +0300

    Remove example usage from cart_item.py

--- shopping-cart-components/src/cart_item.py
+++ shopping-cart-components/src/cart_item.py
@@ -44,38 +44,3 @@ class CartItem:
         """
         discount_amount = self.price * self.discount
         return self.total_price - discount_amount
-
-
-# example use:
-if __name__ == "__main__":
-     # Bu blok yalnızca dosya doğrudan çalıştırıldığında çalışır.
-    # Başka modüller bu sınıfı import etse bile buradaki kodlar çalışmaz.
-
-    item_1 = CartItem(
-        name="Hero-XPulse 200 4V",
-        price=184574.00,
-        link="https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/",
-        discount=0.1,
-    )
-
-    print("Ürün Bilgileri:", item_1)  # __str__ sayesinde otomatik bilgi gösterir.
-    print(f"Ürünlerin Toplam Fiyatı (İndirimsiz)    : {item_1.total_price:.2f}")
-    print(f"Ürünlerin Toplam Fiyatı (İndirimli)     : {item_1.discount_price:.2f}")
-    item_1.quantity += 1
-    print(f"Aynı üründen 1 adet daha eklendi.")
-    print(f"Ürünlerin Toplam Fiyatı (İndirimsiz)    : {item_1.total_price:.2f}")
-    print(f"Ürünlerin Toplam Fiyatı (İndirimli)     : {item_1.discount_price:.2f}")
-
-
-# output:
-# Product Informations: 
-# - Hero-XPulse 200 4V
-# - 184574.00 TL
-# - %10 discount
-# - 1 piece
-# - https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
-# Total Price of Products (Not Discounted)      : 184574.00
-# Total Price of Products (Discounted)          : 166116.60
-# One more of the same product has been added.
-# Total Price of Products (Not Discounted)      : 369148.00
-# Total Price of Products (Discounted)          : 350690.60
