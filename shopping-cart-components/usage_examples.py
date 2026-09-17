4ebc5ada08ebe06d895ff9df221bec423b2ee42e
uruz <uruzutkuun@gmail.com>
2026-09-17 23:33:49 +0300

    Update usage_examples.py

--- shopping-cart-components/usage_examples.py
+++ shopping-cart-components/usage_examples.py
@@ -41,9 +41,8 @@ def demo_single_item(item: CartItem):
     print("\n=== DEMO: SINGLE ITEM ===")
 
     print(item)  # '__str__' metodu çalışır.
-    print(79*"-")
-    print(f"Toplam (indirimsiz): {item.total_price:.2f}")
-    print(f"Toplam (indirimli) : {item.discount_price:.2f}")
+    print(f"\n>>> Toplam (indirimsiz): {item.total_price:.2f}")
+    print(f">>> Toplam (indirimli) : {item.discount_price:.2f}")
 
 
 def demo_shopping_cart(items: list[CartItem]) -> ShoppingCart:
@@ -51,24 +50,26 @@ def demo_shopping_cart(items: list[CartItem]) -> ShoppingCart:
 
     cart = ShoppingCart(items)
     print(cart)  # '__str__' metodu çalışır.
+    print(f"\n>>> Sepet toplam (indirimsiz): {cart.total_price:.2f}")
+    print(f">>> Sepet toplam (indirimli) : {cart.discount_total_price:.2f}")
+
     print(79*"-")
-    print(f"Sepet toplam (indirimsiz): {cart.total_price:.2f}")
-    print(f"Sepet toplam (indirimli) : {cart.discount_total_price:.2f}")
 
     # Örnek işlemler
     cart.remove_item(items[3])
-    print(79*"-")
-    print("-> QJMotor çıkarıldı")
+    print("\n-> QJMotor çıkarıldı")
     print(cart)
+
+    print(79*"-")
 
     cart.add_item(items[0])
-    print(79*"-")
-    print("-> Hero-XPulse eklendi")
+    print("\n-> Hero-XPulse eklendi")
     print(cart)
 
-    cart.update_item_amount(items[0], UpdateAmountAction.INCREASE)
     print(79*"-")
-    print("-> Hero-XPulse adedi artırıldı")
+
+    cart.update_item_amount(items[0], UpdateAmountAction.INCREASE)
+    print("\n-> Hero-XPulse adedi artırıldı")
     print(cart)
 
 
@@ -76,3 +77,136 @@ if __name__ == "__main__":
     items = demo_cart_items()
     demo_single_item(items[0])  # Hero-XPulse 200 4V
     demo_shopping_cart(items)
+
+
+### OUTPUT ###
+"""
+=== DEMO: CART ITEMS ===
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 1 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+- TVS-Raider 125
+- 149400.00 TL
+- %10 indirim
+- 1 adet
+- https://turkiye.tvsmotor.com/tr/p/our-products/tvs-raider-tr
+
+- RKS-Racing R 250
+- 205000.00 TL
+- %15 indirim
+- 1 adet
+- https://www.rksmotor.com.tr/model/r250.html
+
+- QJMotor-SRK 250 RC
+- 180000.00 TL
+- %20 indirim
+- 1 adet
+- https://tr.qjmotor.com/products_details/69.html
+ 
+=== DEMO: SINGLE ITEM ===
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 1 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+>>> Toplam (indirimsiz): 184574.00
+>>> Toplam (indirimli) : 184574.00
+
+=== DEMO: SHOPPING CART ===
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 1 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+- TVS-Raider 125
+- 149400.00 TL
+- %10 indirim
+- 1 adet
+- https://turkiye.tvsmotor.com/tr/p/our-products/tvs-raider-tr
+
+- RKS-Racing R 250
+- 205000.00 TL
+- %15 indirim
+- 1 adet
+- https://www.rksmotor.com.tr/model/r250.html
+
+- QJMotor-SRK 250 RC
+- 180000.00 TL
+- %20 indirim
+- 1 adet
+- https://tr.qjmotor.com/products_details/69.html
+
+>>> Sepet toplam (indirimsiz): 718974.00
+>>> Sepet toplam (indirimli) : 637284.00
+-------------------------------------------------------------------------------
+
+-> QJMotor çıkarıldı
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 1 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+- TVS-Raider 125
+- 149400.00 TL
+- %10 indirim
+- 1 adet
+- https://turkiye.tvsmotor.com/tr/p/our-products/tvs-raider-tr
+
+- RKS-Racing R 250
+- 205000.00 TL
+- %15 indirim
+- 1 adet
+- https://www.rksmotor.com.tr/model/r250.html
+-------------------------------------------------------------------------------
+
+-> Hero-XPulse eklendi
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 2 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+- TVS-Raider 125
+- 149400.00 TL
+- %10 indirim
+- 1 adet
+- https://turkiye.tvsmotor.com/tr/p/our-products/tvs-raider-tr
+
+- RKS-Racing R 250
+- 205000.00 TL
+- %15 indirim
+- 1 adet
+- https://www.rksmotor.com.tr/model/r250.html
+-------------------------------------------------------------------------------
+
+-> Hero-XPulse adedi artırıldı
+
+- Hero-XPulse 200 4V
+- 184574.00 TL
+- %0 indirim
+- 3 adet
+- https://www.heromotor.com.tr/xpulse200-4v-euro5-plus/
+
+- TVS-Raider 125
+- 149400.00 TL
+- %10 indirim
+- 1 adet
+- https://turkiye.tvsmotor.com/tr/p/our-products/tvs-raider-tr
+
+- RKS-Racing R 250
+- 205000.00 TL
+- %15 indirim
+- 1 adet
+- https://www.rksmotor.com.tr/model/r250.html
+"""
